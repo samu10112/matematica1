7bc14267cbad6cd9daf3043b0873e1745bff9dbf
samu10112 <119002488+samu10112@users.noreply.github.com>
2023-03-06 15:41:35 -0300

    Add files via upload

---
+++ math.js
@@ -0,0 +1,14 @@
+var jogador1=localStorage.getItem("player1");
+var jogador2=localStorage.getItem("player2");
+
+var ponto1=0;
+var ponto2=0;
+
+document.getElementById("player1Name").innerHTML=jogador1+":";
+document.getElementById("player2Name").innerHTML=jogador2+":";
+
+document.getElementById("player1score").innerHTML=ponto1;
+document.getElementById("player2score").innerHTML=ponto2;
+
+document.getElementById("turnoDaPergunta").innerHTML="turno da pergunta: "+jogador1;
+document.getElementById("turnoDaResposta").innerHTML="turno da resposta: "+jogador2;
