f10bcd582578c7735908e54f7aee915587e84bc9
samu10112 <119002488+samu10112@users.noreply.github.com>
2023-03-09 16:54:21 -0300

    Add files via upload

--- math.js
+++ math.js
@@ -12,3 +12,15 @@ document.getElementById("player2score").innerHTML=ponto2;
 
 document.getElementById("turnoDaPergunta").innerHTML="turno da pergunta: "+jogador1;
 document.getElementById("turnoDaResposta").innerHTML="turno da resposta: "+jogador2;
+var resposta;
+function enviar_calculo(){
+    var numero1=document.getElementById("numero1").value;
+    var numero2=document.getElementById("numero2").value;
+    var actual_answer=parseInt(numero1)*parseInt(numero2);
+    var question_number="<h4>"+numero1+"x"+numero2+"</h4>";
+    var input_box="<br>resposta:<input type='text'id='input_check_box'>";
+    var check_button="<br><br><button class='btn btn_info' onclick='check()'>checar</button>";
+    var row=question_number+input_box+check_button;
+    document.getElementById("number1").value=" ";
+    document.getElementById("number2").value=" ";
+}
